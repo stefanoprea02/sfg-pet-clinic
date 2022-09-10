85aab7288048e04ac35b4be32986a5fc2fe8ae4f
stefanoprea02 <stefanoprea2002@gmail.com>
2022-09-10 16:50:12 +0300

    Added owner record. Closes #51.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package guru.springframework.sfgpetclinic.controllers;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -17,14 +20,17 @@ public class OwnerController {
 
     @RequestMapping({"","/", "/index", "/index.html"})
     public String listOwners(Model model){
-
         model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
 
-    @RequestMapping("/find")
-    public String findOwners(){
-        return "notimplemented";
+    @GetMapping("/{ownerId}")
+    public ModelAndView findOwners(@PathVariable("ownerId") Long ownerId){
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+
+        mav.addObject(ownerService.findById(ownerId));
+
+        return mav;
     }
 }
